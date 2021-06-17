e01872062ef31506086380e9d2f4804e0a4111ec
dkarnavas <dimitriskarn94@gmail.com>
2021-06-17 22:45:29 +0300

    navbar and carousel fixes

--- src/client/modules/my/carouselWrapper/carouselWrapper.js
+++ src/client/modules/my/carouselWrapper/carouselWrapper.js
@@ -3,12 +3,12 @@ import { LightningElement } from "lwc";
 export default class CarouselWrapper extends LightningElement{
     slides = [
         {
-            image: './resources/Images/Banner_section/home.png',
+            image: './resources/Images/Banner_section/home3.jpg',
             heading:'Welcome!',
             description:'Glad to have you here.'
         },
         {
-            image: './resources/Images/Banner_section/home2.png',
+            image: './resources/Images/Banner_section/home2.jpg',
             heading:'Have fun!',
             description:'Enjoy your time being here.'
         }
