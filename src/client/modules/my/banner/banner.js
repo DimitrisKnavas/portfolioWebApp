dc1f5ed44575fd0450a733f0a6705bd5bab5a7b9
dkarnavas <dimitriskarn94@gmail.com>
2021-06-18 10:32:59 +0300

    add logo in navbar-stuff in banner-delete unused photos

--- src/client/modules/my/banner/banner.js
+++ src/client/modules/my/banner/banner.js
@@ -3,6 +3,4 @@ import { LightningElement } from "lwc";
 export default class Banner extends LightningElement{
     heading = 'Welcome stranger';
     description = 'This is a web app is about myself. Enjoy!'
-
-    
 }
